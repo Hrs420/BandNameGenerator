7a90567772217aca710571bbf0768086063181a3
Hrs420 <116896176+Hrs420@users.noreply.github.com>
2023-07-28 09:26:14 +0530

    Update main.py
    updation in code

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-print("Day 1 - Python Print Function")
-print("The function is decleared like this: ")
-print('what to print')
+print("Welcome to the Band Name Generator ! ")
+
+street = input("Enter the Street you are living in ? ")
+
+pet = input("Enter the name of your Pet ? ")
+
+print("The Band Name would be ", street+pet)
